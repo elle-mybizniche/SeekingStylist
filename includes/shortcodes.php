07553d757b8ce83f09b8f5104efb7e973dce058c
elle-mybizniche <elle@mybizniche.com>
2020-09-22 15:42:53 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -9,7 +9,7 @@ add_shortcode('home_url', 'mbn_shortcode_home_url');
 
 function home_gallery_app_sc($atts = null, $content = null){
 	ob_start();
-    require dirname(__FILE__) .  '\webapps\home-gallery-app.php';
+    require dirname(__FILE__) .  '/webapps/home-gallery-app.php';
     $data = ob_get_contents();
     ob_end_clean();
 
@@ -21,7 +21,7 @@ add_shortcode('home_gallery', 'home_gallery_app_sc');
 
 function featured_stylists_app_sc($atts = null, $content = null){
 	ob_start();
-    require dirname(__FILE__) .  '\webapps\featured-stylists-app.php';
+    require dirname(__FILE__) .  '/webapps/featured-stylists-app.php';
     $data = ob_get_contents();
     ob_end_clean();
 
@@ -33,7 +33,7 @@ add_shortcode('featured_stylists', 'featured_stylists_app_sc');
 
 function latest_articles($atts = null, $content = null){
     ob_start();
-    require dirname(__FILE__) .  '\webapps\latest-articles.php';
+    require dirname(__FILE__) .  '/webapps/latest-articles.php';
     $data = ob_get_contents();
     ob_end_clean();
 
