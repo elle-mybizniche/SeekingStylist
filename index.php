2214070e104a8dde4fdee4bbdd7d18605a8f7019
elle-mybizniche <elle@mybizniche.com>
2020-10-09 12:13:25 +0800

    updates

--- index.php
+++ index.php
@@ -54,22 +54,35 @@
 
 		<div class="pagination">
 		    <?php 
-		        echo paginate_links( array(
+		        $gnPagination = paginate_links( array(
 		            'base'         => str_replace( 999999999, '%#%', esc_url( get_pagenum_link( 999999999 ) ) ),
 		            'total'        => $wp_query->max_num_pages,
 		            'current'      => max( 1, get_query_var( 'paged' ) ),
 		            'format'       => '?paged=%#%',
 		            'show_all'     => false,
-		            'type'         => 'plain',
+		            'type'         => 'array',
 		            'end_size'     => 2,
 		            'mid_size'     => 1,
 		            'prev_next'    => true,
 		            'prev_text'    => sprintf( '<i></i> %1$s', __( 'Newer Posts', 'text-domain' ) ),
-		            'next_text'    => sprintf( '%1$s <i></i>', __( 'Show More', 'text-domain' ) ),
-		            'add_args'     => false,
-		            'add_fragment' => '',
-		        ) ); 
+		            'next_text'    => sprintf( '%1$s <i></i>', __( 'Show More', 'text-domain' ) )
+		        ) );
 		    ?>
+		    <div class="lists">
+		    	<?php foreach ($gnPagination as $item): ?>
+			    	<?php if (!strpos($item, 'next')): ?>
+			    		<?= $item ?>
+			    	<?php endif ?>
+			    <?php endforeach ?>
+		    </div>
+		    <div class="next-btn">
+		    	<?php foreach ($gnPagination as $item): ?>
+			    	<?php if (strpos($item, 'next')): ?>
+			    		<?= $item ?>
+			    	<?php endif ?>
+			    <?php endforeach ?>
+		    </div>
+		    
 		</div>
 	</div>
 	
